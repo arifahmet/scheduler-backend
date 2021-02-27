03ef52713d8fed0bd3fcf5962d638600bdd25e64
Arif Ahmet <arifahmetbarbak@gmail.com>
2021-02-27 13:18:21 +0300

    add cross origin

--- src/main/java/com/barbaktech/schedulerbackend/controller/v1/UserController.java
+++ src/main/java/com/barbaktech/schedulerbackend/controller/v1/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
     private ClientRegistration registration;
 
